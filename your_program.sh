d2c620222fa88deaa40e503b0d5c1a05f7a00067
APNaik <arya.naik2517@gmail.com>
2026-07-04 00:22:18 +0530

    codecrafters submit [skip ci]

--- your_program.sh
+++ your_program.sh
@@ -8,13 +8,19 @@
 
 set -e # Exit early if any commands fail
 
+toolchain_args=""
+if [ -n "$VCPKG_ROOT" ] && [ -f "$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake" ]; then
+  toolchain_args="-DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
+fi
+
 # Copied from .codecrafters/compile.sh
 #
 # - Edit this to change how your program compiles locally
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+  rm -rf build
+  cmake -B build -S . $toolchain_args
   cmake --build ./build
 )
 
