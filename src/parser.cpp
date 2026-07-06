330e8418f9a4b624f1d959a91b3c03b31d0226bc
APNaik <arya.naik2517@gmail.com>
2026-07-07 00:55:58 +0530

    codecrafters submit [skip ci]

--- src/parser.cpp
+++ src/parser.cpp
@@ -11,6 +11,52 @@ std::size_t find_first_space(const std::string& text, std::size_t start) {
   return text.find(' ', start);
 }
 
+bool parse_token(const std::string &input, std::size_t &idx, std::string &token) {
+  bool inside_single_quote { false };
+  bool inside_double_quote { false };
+
+  while (idx < input.size()) {
+    const char current { input[idx] };
+
+    if (current == '\\' && idx + 1 < input.size()) {
+      const char next { input[idx + 1] };
+
+      if (!inside_double_quote && !inside_single_quote) {
+        token.push_back(input[++idx]);
+        ++idx;
+        continue;
+      }
+
+      if (inside_double_quote && (next == '"' || next == '\\')) {
+        token.push_back(input[++idx]);
+        ++idx;
+        continue;
+      }
+    }
+
+    if (current == '\'' && !inside_double_quote) {
+      inside_single_quote = !inside_single_quote;
+      ++idx;
+      continue;
+    }
+
+    if (current == '"' && !inside_single_quote) {
+      inside_double_quote = !inside_double_quote;
+      ++idx;
+      continue;
+    }
+
+    if (current == ' ' && !inside_single_quote && !inside_double_quote) {
+      break;
+    }
+
+    token.push_back(current);
+    ++idx;
+  }
+
+  return !inside_single_quote && !inside_double_quote;
+}
+
 }  // empty namespace keeps the functions private to the current file
 
 Command parse_command(const std::string& input) {
@@ -22,10 +68,13 @@ Command parse_command(const std::string& input) {
     return command;
   }
 
-  const std::size_t first_space { find_first_space(input, first_non_space) };
-  command.raw = input.substr(first_non_space, first_space - first_non_space);
+  std::size_t argument_start { first_non_space };
+  if (!parse_token(input, argument_start, command.raw)) {
+    std::cerr << "Quotes not closed properly" << std::endl;
+    command.empty = true;
+    return command;
+  }
 
-  std::size_t argument_start { first_space };
   while (argument_start != std::string::npos) {
     argument_start = input.find_first_not_of(' ', argument_start);
     if (argument_start == std::string::npos) {
@@ -34,57 +83,13 @@ Command parse_command(const std::string& input) {
 
     std::string argument;
 
-    // To handle single and double quoted arguments
-    bool inside_single_quote { false };
-    bool inside_double_quote { false };
-
-    while (argument_start < input.size()) {
-      const char current { input[argument_start] };
-
-      if (current == '\\' && argument_start + 1 < input.size()) {
-        const char next { input[argument_start + 1] };
-
-        if (!inside_double_quote && !inside_single_quote) {
-          argument.push_back(input[++argument_start]);
-          ++argument_start;
-          continue;
-        }
-
-        if (inside_double_quote && (next == '"' || next == '\\')) {
-          argument.push_back(input[++argument_start]);
-          ++argument_start;
-          continue;
-        }
-      }
-
-      if (current == '\'' && !inside_double_quote) {
-        inside_single_quote = !inside_single_quote;
-        ++argument_start;
-        continue;
-      }
-
-      if (current == '"' && !inside_single_quote) {
-        inside_double_quote = !inside_double_quote;
-        ++argument_start;
-        continue;
-      }
-
-      if (current == ' ' && !inside_single_quote && !inside_double_quote) {
-        break;
-      }
-
-      argument.push_back(current);
-      ++argument_start;
-    }
-
-    if (inside_single_quote || inside_double_quote) {
+    if (!parse_token(input, argument_start, argument)) {
       std::cerr << "Quotes not closed properly" << std::endl;
       command.empty = true;
       return command;
     }
 
     command.arguments.push_back(argument);
-    argument_start = find_first_space(input, argument_start);
   }
 
   return command;
