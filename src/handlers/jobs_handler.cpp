9ec5aa5205bba506dbb6afb1dd9be8e1a21722ba
APNaik <arya.naik2517@gmail.com>
2026-07-17 23:59:02 +0530

    Reaping background jobs

--- src/handlers/jobs_handler.cpp
+++ src/handlers/jobs_handler.cpp
@@ -3,13 +3,19 @@
 
 #include <iomanip>
 #include <iostream>
+#include <sys/wait.h>
+#include <algorithm>
 
 std::vector<Job> active_background_jobs;
 
 void handle_jobs(const Command &command){
-  for(size_t i { 0 }; i<active_background_jobs.size(); ++i){
-    const auto& job = active_background_jobs[i];
+  std::vector<Job> next_cycle_jobs;
 
+  for(size_t i { 0 }; i<active_background_jobs.size(); ++i){
+    auto& job = active_background_jobs[i];
+    int status;
+    pid_t result = waitpid(job.pid, &status, WNOHANG);   // WNOHANG checks the process status instantly without halting shell thread
+    
     char marker {};
     if(i == active_background_jobs.size() - 1){
       marker = '+';
@@ -18,9 +24,33 @@ void handle_jobs(const Command &command){
       marker = '-';
     }
     else marker = ' ';
-    
-    std::cout << "[" << job.job_number << "]" << marker << " "
-              << std::left << std::setw(24) << job.status
-              << job.command_string << "\n"; 
+
+    if(result == 0){    // Process is still executing
+      char marker {};
+      if(i == active_background_jobs.size() - 1){
+        marker = '+';
+      }
+      else if(i == active_background_jobs.size() - 2){
+        marker = '-';
+      }
+      else marker = ' ';
+      
+      std::cout << "[" << job.job_number << "]" << marker << " "
+                << std::left << std::setw(24) << job.status
+                << job.command_string << "\n"; 
+      next_cycle_jobs.push_back(job);
+    }
+    else if(result > 0){    // Process is exited
+      job.status = "Done";
+      std::string display_cmd = job.command_string;
+      if (display_cmd.ends_with(" &")) {
+        display_cmd.erase(display_cmd.length() - 2);
+      }
+            
+      std::cout << "[" << job.job_number << "]" << marker << " "
+                << std::left << std::setw(24) << job.status
+                << display_cmd << "\n";
+    }
   }
+  active_background_jobs = std::move(next_cycle_jobs);
 }
