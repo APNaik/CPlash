51a551611322774ab3a2dab0db537c3ad836b937
APNaik <arya.naik2517@gmail.com>
2026-07-21 17:31:47 +0530

    Added multiple commands pipeline

--- src/execute_pipeline.cpp
+++ src/execute_pipeline.cpp
@@ -81,48 +81,49 @@ void run_pipeline_stage(const Command& command, int fdin, int fdout){
 } // namespace
 
 void execute_pipeline(const Pipeline& pipeline){
-  if(pipeline.commands.size() < 2) return;
+  const size_t num_cmds {pipeline.commands.size() };
+  if(num_cmds < 2) return;
+  
+  std::vector<pid_t> pids;
+  int prev_fd = STDIN_FILENO;
+  
+  for(size_t i {0}; i < num_cmds; ++i){
+    int curr_pipe[2] = {-1, -1}; // 0 -> read end and 1 -> write end
 
-  int pipefd[2];   // 0 -> read end and 1 -> write end
-  if(pipe(pipefd) == -1){
-    std::perror("pipe");
-    return;
-  }
-  const auto& left_command { pipeline.commands[0] };
-  const auto& right_command { pipeline.commands[1] };
-  pid_t pid1 = fork();
-  if (pid1 == -1) {
-    std::perror("fork");
-    close(pipefd[0]);
-    close(pipefd[1]);
-    return;
-  }
-  if(pid1 == 0){
-    // Child 1 does not read from the pipe
-    close(pipefd[0]);
-    run_pipeline_stage(left_command, STDIN_FILENO, pipefd[1]);
-  }
+    if(i < num_cmds - 1){
+      if(pipe(curr_pipe) == -1){
+        std::perror("pipe");
+        if(prev_fd != STDIN_FILENO) close(prev_fd);
+        return;
+      }
+    }
+    pid_t pid { fork() };
+    if(pid == -1){
+      std::perror("fork");
+      if(prev_fd != STDIN_FILENO) close(prev_fd);
+      if(curr_pipe[0] != -1) close(curr_pipe[0]);
+      if(curr_pipe[1] != -1) close(curr_pipe[1]);
+      break;
+    }
+    if(pid == 0){
+      int fdin { (i == 0) ? STDIN_FILENO : prev_fd };
+      int fdout { (i == num_cmds - 1) ? STDOUT_FILENO : curr_pipe[1] };
+      // new child does not read from the pipe
+      if(curr_pipe[0] != -1) close(curr_pipe[0]);
+      run_pipeline_stage(pipeline.commands[i], fdin, fdout);
+    }
 
-  pid_t pid2 = fork();
-  if(pid2 == -1) {
-    std::perror("fork");
-    close(pipefd[0]);
-    close(pipefd[1]);
+    pids.push_back(pid);
+    if(prev_fd != STDIN_FILENO){
+      close(prev_fd);
+    }
+    if(i < num_cmds - 1){
+      close(curr_pipe[1]);  // Crucial: close write-end in parent so reader receives EOF
+      prev_fd = curr_pipe[0];  // Save read-end for command i + 1
+    }
+  } 
+  for (pid_t pid : pids) {
     int status;
-    waitpid(pid1, &status, 0);
-    return;
+    waitpid(pid, &status, 0);
   }
-  if(pid2 == 0){
-    // Child 2 does not write to pipe
-    close(pipefd[1]);
-    run_pipeline_stage(right_command, pipefd[0], STDOUT_FILENO);
-  }
-
-  // Close all the pipe descriptors for parent process
-  close(pipefd[0]);
-  close(pipefd[1]);
-
-  int status;
-  waitpid(pid1, &status, 0);
-  waitpid(pid2, &status, 0);
 }
